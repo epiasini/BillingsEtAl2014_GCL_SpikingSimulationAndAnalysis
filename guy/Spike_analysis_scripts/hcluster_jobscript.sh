4d2201668d363a7cadeeded914f426c6a411bd5e
Eugenio Piasini <e.piasini@ucl.ac.uk>
2011-05-10 11:46:18 +0100

    Adapted hcluster_jobscript to run the MATLAB clustering and analysis scripts in a loop over bias values.

--- guy/Spike_analysis_scripts/hcluster_jobscript.sh
+++ guy/Spike_analysis_scripts/hcluster_jobscript.sh
@@ -5,5 +5,7 @@
 #$ -e $HOME/log/
 ##$ -l nodes=2:ppn=8
 
+bias=$1
+
 cd $HOME/code/network/trunk/guy/Spike_analysis_scripts
-matlab -nojvm -r HCluster; exit
+matlab -nojvm -r "bias=$bias;HCluster;exit"
