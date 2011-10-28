62ce2ddd3058458087b0aa08fedb6e7570b76f32
Eugenio Piasini <e.piasini@ucl.ac.uk>
2011-10-28 15:12:19 +0100

    Fixed a bug in the process manager's historical archive of commands.
    This meant that the master_script failed every time that it had to delete and reschedule a job because of the ConcurrentModificationException.

--- master_script.py
+++ master_script.py
@@ -30,18 +30,20 @@ class ProcessManager(object):
         return set(int(l.split()[0]) for l in stat_lines[2:] if len(l)>0).intersection(self.__managed_jobs)
         
     def submit_job(self, qsub_argument_list):
-        popen_command = itertools.chain(['qsub'], qsub_argument_list)
+        popen_command = list(itertools.chain(['qsub'], qsub_argument_list))
+        print(popen_command)
         handle = Popen(popen_command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-#        if handle.returncode!=0:
-#            raise QueueError()
         jid = int((handle.communicate()[0]).split(' ')[2])
-        self.__qsub_commands[jid] = qsub_argument_list
+        if handle.returncode!=0:
+            raise QueueError()
+        self.__qsub_commands[jid] = popen_command[1:]
         self.__managed_jobs.add(jid)
         self.update_job_sets()
-        return jid
+        print('Submitted job {jid}'.format(jid=jid))
     def delete_job(self, jid):
         call(['qdel', str(jid)])
         self.update_job_sets()
+        print('Deleted job {jid}'.format(jid=jid))
     def update_job_sets(self):
         self.running_jobs = self.__managed_jids_from_qstat('-s', 'r')
         self.waiting_jobs = self.__managed_jids_from_qstat('-s', 'p')
@@ -52,9 +54,9 @@ class ProcessManager(object):
             try:
                 with open('/home/ucbtepi/log/simulate_jobscript.sh.e{jid}'.format(jid=jid), 'r') as f:
                     if 'ConcurrentModificationException' in f.read():
+                        print('Found ConcurrentModificationException in job {jid}'.format(jid=jid))
                         self.delete_job(jid)
-                        new_jid = self.submit_job(self.__qsub_commands[jid])
-                        print('Found ConcurrentModificationException in job {old_jid}. Deleting job and resubmitted as {new_jid}.'.format(old_jid=jid, new_jid=new_jid))
+                        self.submit_job(self.__qsub_commands[jid])
             except IOError as (errno, strerror):
                 print('I/O Error {errno}:{strerror}'.format(errno=errno, strerror=strerror))
 
