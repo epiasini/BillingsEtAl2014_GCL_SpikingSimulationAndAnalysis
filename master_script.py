f20bb5be4d26ce89da04a419e5d7eef8994c324c
Eugenio Piasini <e.piasini@ucl.ac.uk>
2011-10-28 16:51:13 +0100

    Moved the ProcessManager to its own module in utils.

--- master_script.py
+++ master_script.py
@@ -5,60 +5,15 @@ import time
 import random
 import itertools
 from math import factorial
-from subprocess import Popen, PIPE, call
 
 from utils.paths import data_folder_path_ctor, data_archive_path_ctor, conn_pattern_filename, stim_pattern_filename
-
-class QueueError(Exception):
-    pass
+from utils.queue import ProcessManager
 
 class NetworkSizeError(Exception):
     def __init__(self, value):
         self.value = value
     def __str__(self):
         return repr(self.value)
-
-class ProcessManager(object):
-    def __init__(self):
-        self.__managed_jobs = set() # all the jobs in the queue that are being managed by this manager
-        self.running_jobs = set() # all (managed) running jobs
-        self.waiting_jobs = set() # all (managed) waiting jobs
-        self.other_jobs = set() # all the other managed jobs (e.g. jobs that are being transferred)
-        self.__qsub_commands = dict() # historical record of the qsub commands that generated each managed job
-    def __managed_jids_from_qstat(self, *qstat_argument_list):
-        stat_lines = Popen(itertools.chain(['qstat'], qstat_argument_list), stdout=PIPE, stderr=PIPE).communicate()[0].split('\n')
-        return set(int(l.split()[0]) for l in stat_lines[2:] if len(l)>0).intersection(self.__managed_jobs)
-        
-    def submit_job(self, qsub_argument_list):
-        popen_command = list(itertools.chain(['qsub'], qsub_argument_list))
-        print(popen_command)
-        handle = Popen(popen_command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        jid = int((handle.communicate()[0]).split(' ')[2])
-        if handle.returncode!=0:
-            raise QueueError()
-        self.__qsub_commands[jid] = popen_command[1:]
-        self.__managed_jobs.add(jid)
-        self.update_job_sets()
-        print('Submitted job {jid}'.format(jid=jid))
-    def delete_job(self, jid):
-        call(['qdel', str(jid)])
-        self.update_job_sets()
-        print('Deleted job {jid}'.format(jid=jid))
-    def update_job_sets(self):
-        self.running_jobs = self.__managed_jids_from_qstat('-s', 'r')
-        self.waiting_jobs = self.__managed_jids_from_qstat('-s', 'p')
-        self.other_jobs = set(jid for jid in self.__managed_jids_from_qstat() if jid not in self.running_jobs.union(self.waiting_jobs))
-    def update_and_check_for_CME(self):
-        self.update_job_sets()
-        for jid in self.running_jobs:
-            try:
-                with open('/home/ucbtepi/log/simulate_jobscript.sh.e{jid}'.format(jid=jid), 'r') as f:
-                    if 'ConcurrentModificationException' in f.read():
-                        print('Found ConcurrentModificationException in job {jid}'.format(jid=jid))
-                        self.delete_job(jid)
-                        self.submit_job(self.__qsub_commands[jid])
-            except IOError as (errno, strerror):
-                print('I/O Error {errno}:{strerror}'.format(errno=errno, strerror=strerror))
 
 #++++general controls+++++
 simulate = True
@@ -73,21 +28,21 @@ sim_config_name = 'Default Simulation Configuration' # hardcoded in simulate.py
 nC_seed = 1234 # hardcoded in simulate.py
 sim_duration = 300.0 # hardcoded in simulate.py
 n_stim_patterns = 20
-n_trials = 500
+n_trials = 100
 min_mf_number = 6
 grc_mf_ratio = 3.
 #+++++parameter ranges+++++++++++++
 n_grc_dend_range = [4]
 network_scale_range = [1.66]
 active_mf_fraction_range = [0.5]
-bias_range = [0., -10., -25., -45.]
+bias_range = [0., -10.]
 #++++++++++++++++++++++++++
 
 ranges = [n_grc_dend_range, network_scale_range, active_mf_fraction_range, bias_range]
 parameter_space = itertools.product(*ranges)
 
 #----parallel settings----
-size_per_simulation = 10
+size_per_simulation = 20
 
 #----parameter consistency control
 min_nmf = round(min(network_scale_range)*min_mf_number)
@@ -103,8 +58,6 @@ process_manager = ProcessManager()
 ############################
 ##====SIMULATION STAGE====##
 ############################
-initial_jobs = set()
-
 if simulate:
     for sim_dict in master_list:
         n_grc_dend = sim_dict['params'][0]
@@ -155,7 +108,7 @@ if simulate:
                 qsub_argument_list = itertools.chain(['simulate_jobscript.sh', str(min_mf_number), str(grc_mf_ratio)], [str(x) for x in sim_dict['params']], [str(n_stim_patterns), str(n_trials), str(size_per_simulation), str(rank)])
                 process_manager.submit_job(qsub_argument_list)
                 
-    while process_manager.running_jobs or process_manager.waiting_jobs or process_manager.other_jobs:
+    while process_manager.queue_is_not_empty():
         # check for jobs that may have stalled due to the dreaded ConcurrentModificationException by parsing the error log files, since I don't seem to be able to catch that exception at the jython level.
         process_manager.update_and_check_for_CME()
         print('{rj} running, {wj} waiting, {oj} other jobs'.format(rj=len(process_manager.running_jobs), wj=len(process_manager.waiting_jobs), oj=len(process_manager.other_jobs)))
@@ -181,7 +134,7 @@ if compress:
             process_manager.submit_job(qsub_argument_list)            
             #popen_command = itertools.chain(['qsub', 'compress_jobscript.sh'], [str(min_mf_number), str(grc_mf_ratio)], [str(x) for x in sim_dict['params']], [str(n_stim_patterns), str(n_trials)], [str(int(clean_up_after_compress))])
     
-    while process_manager.running_jobs or process_manager.waiting_jobs or process_manager.other_jobs:
+    while process_manager.queue_is_not_empty():
         process_manager.update_job_sets()
         print('{rj} running, {wj} waiting, {oj} other_jobs'.format(rj=len(process_manager.running_jobs), wj=len(process_manager.waiting_jobs), oj=len(process_manager.other_jobs)))
         time.sleep(60)
