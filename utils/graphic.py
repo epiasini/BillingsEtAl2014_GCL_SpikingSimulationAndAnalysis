d666ce5612348de4ab3a130161cccc895a75f43d
Eugenio Piasini <e.piasini@ucl.ac.uk>
2011-10-28 16:57:21 +0100

    Now using the 'coolwarm' colormap. Requires matplotlib >= 1.1.0.

--- utils/graphic.py
+++ utils/graphic.py
@@ -7,7 +7,7 @@ def plot_data_vector(axes, data_vector, convolve_dt=2):
         return int(x*convolve_dt)
     conv_ax = axes
     conv_ax.xaxis.set_major_formatter(FuncFormatter(scale_by_convolve_dt))
-    conv_plot = conv_ax.imshow(data_vector, cmap='hot', interpolation='nearest', aspect=2, origin='lower')
+    conv_plot = conv_ax.imshow(data_vector, cmap='coolwarm', interpolation='none', aspect=2, origin='lower')
     conv_ax.set_xlabel('Time (ms)')
     conv_ax.set_ylabel('Cell index')
     return conv_plot
