d7eb9a7c7295069ad3cbc390ba460a9ecb6d1bd9
Eugenio Piasini <e.piasini@ucl.ac.uk>
2012-02-27 14:51:25 +0000

    Clean-up: deleted the folder containing guy's old scripts.

--- guy/Spike_analysis_scripts/c_dev/testdecode.m
+++
@@ -1,9 +0,0 @@
-% script to test decodeFST
-
-load test.mat
-debug=0;
-clusters=10;
-bdims=size(book_vector);
-bdims=bdims(2);
-
-alphabetFST=decodeFST(observations,clusters,book_vector,squeeze(vector(:,test_chunk,:)),bdims,debug)
