f540597a115aa3099bbc171404bb6adf1bf02238
Eugenio Piasini <e.piasini@ucl.ac.uk>
2013-01-28 15:38:57 +0000

    Add lower triangular index transformation.

--- utils/golgi_maxent.py
+++ utils/golgi_maxent.py
@@ -1,9 +1,17 @@
 import numpy as np
 
 def index_1d(n, row, col):
-    '''Return linear index for an upper triangular matrix element.'''
-    if col <= row:
-	raise ValueError, '{0}, {1}'.format(row ,col)
+    '''
+    Return linear index for an upper triangular matrix element. If a
+    lower triangular element (i,j: j<i) is given, the linear index for
+    (j,i) will be returned instead.
+    '''
+    if col < row:
+	col_old = col
+	col = row
+	row = col_old
+    elif col == row:
+	raise ValueError, "Entries on the diagonal are not represented."
     return n*row + col - (row+1)*(row+2)/2
 
 def index_2d(n,t):
@@ -23,10 +31,16 @@ def index_2d(n,t):
 def test(n=5):
     # matrix -> vector -> matrix
     for r in range(n):
-	for c in range(r+1, n):
+	for c in range(r) + range(r+1, n):
 	    t = index_1d(n,r,c)
 	    print '{}, -> {} -> {}'.format((r, c), t, index_2d(n, t))
-	    assert (r,c) == index_2d(n, t)
+	    if c < r:
+		# if on the lower triangle, transforming to vector
+		# notation and back should bring us on the upper
+		# triangle
+		assert (c, r) == index_2d(n, t)
+	    else:
+		assert (r, c) == index_2d(n, t)
     # vector -> matrix -> vector
     for t in range(n*(n-1)/2):
 	r, c = index_2d(n, t)
