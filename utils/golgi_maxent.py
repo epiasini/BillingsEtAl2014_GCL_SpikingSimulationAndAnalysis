4c1252073e9008991a8d59c4e1c8f7c1f44ecac3
Eugenio Piasini <e.piasini@ucl.ac.uk>
2013-01-29 17:25:12 +0000

    First attempt at exact computation of constraint expressions.

--- utils/golgi_maxent.py
+++ utils/golgi_maxent.py
@@ -29,17 +29,23 @@ def index_2d(n,t):
     return row,col
 
 def matrix_form(n, v):
-    a = np.zeros((n,n))
+    a = np.zeros((n,n), dtype=np.bool)
     for (r,c) in [(r,c) for r in range(n) for c in range(n) if r!=c]:
 	a[r,c] = v[index_1d(n,r,c)]
     return a
 
 def vector_form(a):
     n = a.shape[0]
-    v = np.zeros(n*(n-1)/2)
+    v = np.zeros(n*(n-1)/2, dtype=np.bool)
     for t in range(v.shape[0]):
 	v[t] = a[index_2d(n, t)]
     return v
+
+def vector_from_string(s):
+    return np.array([int(x) for x in s], dtype=np.bool)
+
+def vector_from_int(n, k):
+    return vector_from_string(np.binary_repr(k, width=n*(n-1)/2))
 
 def gamma(n, v):
     '''Variance of the degree distribution of the network.'''
@@ -49,6 +55,24 @@ def pdf(n, v, lamb, mu, nu):
     # mu*v + nu*(1-v) = f_t(v_t) (remember that v is binary)
     # mu and nu are n(n-1)/2 dimensional parameter vectors. lambda is scalar.
     return (mu*v + nu*(1-v)).prod() * np.exp(lamb*gamma(n, v))
+
+def tth_marginal(n, t, lamb, mu, nu):
+    full_space = np.arange(2**(n*(n-1)/2))
+    print [np.binary_repr(v, width=n*(n-1)/2) for v in full_space]
+    probabilities = np.array([pdf(n,vector_from_int(n, k),lamb,mu,nu) for k in full_space])
+    print probabilities
+    print probabilities.sum()
+    #value_in_one = (np.bitwise_and(space, 2**t)/2**t).dot(probabilities)
+    #return value_in_one
+    def marginal(n, v_t):
+	pass
+
+def marginal_contraint_exact(required_marginal_v):
+    pass
+
+def constraint_expression(x, n, required_marginal_v, required_gamma):
+    lamb, mu, nu = x
+    pass
 
 if __name__ == "__main__":
     n = 5
@@ -91,7 +115,7 @@ def test_index(n=5):
 
 def test_transformation(n=5):
     # matrix -> vector -> matrix
-    a = np.random.random(size=(n,n))
+    a = np.random.random(size=(n,n), dtype=np.bool)
     a = np.abs((a - np.transpose(a))/2) # symmetrise
     print a
     print vector_form(a)
@@ -99,7 +123,7 @@ def test_transformation(n=5):
     assert (a == matrix_form(n, vector_form(a))).all()
 
     # vector -> matrix -> vector
-    v = np.random.random(size=n*(n-1)/2)
+    v = np.random.random(size=n*(n-1)/2, dtype=np.bool)
     print v
     print matrix_form(n, v)
     print vector_form(matrix_form(n, v))
