776b691a8d834ce7d8a0e90594c6580c4838a29f
Eugenio Piasini <e.piasini@ucl.ac.uk>
2013-01-29 10:06:28 +0000

    Add matrix<->vector form transformations and gamma function (variance of degree distribution).

--- utils/golgi_maxent.py
+++ utils/golgi_maxent.py
@@ -28,21 +28,68 @@ def index_2d(n,t):
     col = (row + 1) + t - (tot - entries)
     return row,col
 
-def test(n=5):
+def matrix_form(n, v):
+    a = np.zeros((n,n))
+    for (r,c) in [(r,c) for r in range(n) for c in range(n) if r!=c]:
+	a[r,c] = v[index_1d(n,r,c)]
+    return a
+
+def vector_form(a):
+    n = a.shape[0]
+    v = np.zeros(n*(n-1)/2)
+    for t in range(v.shape[0]):
+	v[t] = a[index_2d(n, t)]
+    return v
+
+def gamma(n, v):
+    '''Variance of the degree distribution of the network.'''
+    a = matrix_form(n, v)
+    return np.square(a.sum(axis=0)).sum()/n - np.square(a.sum()/n)
+
+
+
+if __name__ == "__main__":
+    n = 5
+    marginal_v = np.array([0.59 , 0.38, 0.33, 0.39, 0.11, 0.70, 0.78, 0.36, 0.56, 0.74])
+    print matrix_form(n, marginal_v)
+    print gamma(n, marginal_v)
+
+
+
+def test_index(n=5):
     # matrix -> vector -> matrix
     for r in range(n):
-	for c in range(r) + range(r+1, n):
-	    t = index_1d(n,r,c)
-	    print '{}, -> {} -> {}'.format((r, c), t, index_2d(n, t))
-	    if c < r:
-		# if on the lower triangle, transforming to vector
-		# notation and back should bring us on the upper
-		# triangle
-		assert (c, r) == index_2d(n, t)
+	for c in range(n):
+	    if c == r:
+		pass
 	    else:
-		assert (r, c) == index_2d(n, t)
+		t = index_1d(n,r,c)
+		print '{}, -> {} -> {}'.format((r, c), t, index_2d(n, t))
+		if c < r:
+		    # if on the lower triangle, transforming to vector
+		    # notation and back should bring us on the upper
+		    # triangle
+		    assert (c, r) == index_2d(n, t)
+		else:
+		    assert (r, c) == index_2d(n, t)
     # vector -> matrix -> vector
     for t in range(n*(n-1)/2):
 	r, c = index_2d(n, t)
 	print '{} -> {} -> {}'.format(t, (r, c), index_1d(n, r, c))
 	assert t == index_1d(n, r, c)
+
+def test_transformation(n=5):
+    # matrix -> vector -> matrix
+    a = np.random.random(size=(n,n))
+    a = np.abs((a - np.transpose(a))/2) # symmetrise
+    print a
+    print vector_form(a)
+    print matrix_form(n, vector_form(a))
+    assert (a == matrix_form(n, vector_form(a))).all()
+
+    # vector -> matrix -> vector
+    v = np.random.random(size=n*(n-1)/2)
+    print v
+    print matrix_form(n, v)
+    print vector_form(matrix_form(n, v))
+    assert (v == vector_form(matrix_form(n, v))).all()
