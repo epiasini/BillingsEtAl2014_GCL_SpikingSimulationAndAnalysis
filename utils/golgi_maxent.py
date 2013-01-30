279d25a2a747abb1e3264debf06040f8a5766d89
Eugenio Piasini <e.piasini@ucl.ac.uk>
2013-01-30 12:16:28 +0000

    Computing marginals now works.

--- utils/golgi_maxent.py
+++ utils/golgi_maxent.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.stats as st
 
 def index_1d(n, row, col):
     '''
@@ -52,20 +53,47 @@ def gamma(n, v):
     return matrix_from_vector(n, v).sum(axis=0).var()
 
 def pdf(n, v, lamb, mu, nu):
-    # mu*v + nu*(1-v) = f_t(v_t) (remember that v is binary)
+    # mu[t]*(1-v[t]) + nu[t]*v[t] = f_t(v_t) (remember that v is binary)
     # mu and nu are n(n-1)/2 dimensional parameter vectors. lambda is scalar.
-    return (mu*v + nu*(1-v)).prod() * np.exp(lamb*gamma(n, v))
+    return (mu*(1-v) + nu*v).prod() * np.exp(lamb*gamma(n, v))
 
-def tth_marginal(n, t, lamb, mu, nu):
+def margins(n, lamb, mu, nu):
+    assert mu.shape[0] == nu.shape[0]
+    assert mu.shape[0] == n*(n-1)/2
+    # each 'adjacency vector' (vector form of an adjacency matrix)
+    # corresponds to the binary representation of a number between 0
+    # and 2**(n*(n-1)/2)
     full_space = np.arange(2**(n*(n-1)/2))
-    print [np.binary_repr(v, width=n*(n-1)/2) for v in full_space]
-    probabilities = np.array([pdf(n,vector_from_int(n, k),lamb,mu,nu) for k in full_space])
-    print probabilities
-    print probabilities.sum()
-    #value_in_one = (np.bitwise_and(space, 2**t)/2**t).dot(probabilities)
+    # print [np.binary_repr(v, width=n*(n-1)/2) for v in full_space]
+    probabilities = np.array([pdf(n,vector_from_int(n, m),lamb,mu,nu) for m in full_space]).reshape([2 for each in range(n*(n-1)/2)])
+
+    # probabilities = np.array([pdf(n,vector_from_int(n, m),lamb,mu,nu) for m in full_space])
+    # margin_table = np.zeros(shape=(n*(n-1)/2, 2))
+    # for t in range(n*(n-1)/2):
+    # 	# t is a big endian index if the binary vector v is viewed as a
+    # 	# number, but below we need the little endian version
+    # 	k = n*(n-1)/2 - 1 - t
+    # 	# the marginal is defined over {0,1}, so it can be represented with a vector of size two.
+    # 	marginal = np.zeros(2)
+    # 	# divide the full space of all possible adjacency matrices into
+    # 	# according to the presence of the egde number t ('tth' edge).
+    # 	idxs_where_tth_0 = (1 - np.bitwise_and(full_space, (2**k))/(2**k)).nonzero()
+    # 	idxs_where_tth_1 = (np.bitwise_and(full_space, (2**k))/(2**k)).nonzero()
+    # 	print idxs_where_tth_0
+    # 	print idxs_where_tth_1
+    # 	marginal[False] = probabilities[idxs_where_tth_0].sum()
+    # 	marginal[True] = probabilities[idxs_where_tth_1].sum()
+    # 	margin_table[t] = marginal
+
+    return np.array([np.squeeze(p) for p in st.contingency.margins(probabilities)])
+
+    # print probabilities
+    # print probabilities.sum()
+    # print probabilities.max(), probabilities.min()
+    # print probabilities[0,1,0,0,0,1]
+    # print probabilities[1,0,1,1,1,0]
+    # print probabilities[0,0,1,1,1,0]
     #return value_in_one
-    def marginal(n, v_t):
-	pass
 
 def marginal_contraint_exact(required_marginal_v):
     pass
@@ -79,8 +107,8 @@ if __name__ == "__main__":
     marginal_v = np.array([0.59 , 0.38, 0.33, 0.39, 0.11, 0.70, 0.78, 0.36, 0.56, 0.74])
     # our initial guess for the solution is just a product of marginals
     initial_lamb = 0
-    initial_mu = marginal_v
-    initial_nu = 1 - initial_mu
+    initial_nu = marginal_v
+    initial_mu = 1 - initial_nu
     # v = np.random.randint(low=0, high=2, size=n*(n-1)/2)
     v1 = np.array([1, 1, 0, 0, 0, 1, 1, 0, 0, 0])
     v2 = np.array([1, 0, 0, 0, 0, 1, 1, 0, 1, 1])
