f61815d4c349f7e534e10e7677198778af259d3d
Eugenio Piasini <e.piasini@ucl.ac.uk>
2013-12-24 19:52:51 +0000

    Update data directory creation script for new param space structure.

--- scripts/create_network_structures.sh
+++ scripts/create_network_structures.sh
@@ -2,26 +2,14 @@
 # network structure files to their locations.
 
 data_dir=/home/ucbtepi/code/network/data
-gd_range="1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-15
-20"
 
-for gd in $gd_range
+for gd in {1..20}
 do
     origin_dir=$data_dir/network_structures/gd$gd
-    target_dir=$data_dir/gmr2.90/gd$gd/s28.74
-    mkdir -p $target_dir
-    echo copying $origin_dir/GCLconnectivity_full.graphml to $target_dir/gmr2.90_gd"$gd"_s28.74.graphml
-    cp $origin_dir/GCLconnectivity_full.graphml $target_dir/gmr2.90_gd"$gd"_s28.74.graphml
+    mkdir -p $data_dir/gd$gd/cr0
+    mkdir -p $data_dir/gd$gd/cr1
+    cp -v $origin_dir/GCLconnectivity_full.graphml $data_dir/gd$gd/cr0/gd"$gd"_cr0.graphml
+    cp -v $origin_dir/GCLconnectivity_full_randomised.graphml $data_dir/gd$gd/cr1/gd"$gd"_cr1.graphml
 done
 
 exit 0
