8b12ecb505f37acb98ccec2b3f38d8512bd7db0c
Eugenio Piasini <e.piasini@ucl.ac.uk>
2014-01-28 18:55:28 +0000

    Master script: small improvements to flexibility + new default options.

--- master_script.py
+++ master_script.py
@@ -12,34 +12,34 @@ clean_up_simulation_files = True
 
 simulate = True
 compress = True
-analyse = True
+analyse = False
 
 #+++++parameter ranges+++++++++++++
-n_grc_dend = psl(1, 11, 1)
+n_grc_dend = psl(1,21,1)
 connectivity_rule = psl(0) # 0: tissue model, 1: random bipartite graph
 input_spatial_correlation_scale = psl(0) # 0: uncorrelated
 active_mf_fraction = psl(.1,1.,.1)
 gaba_scale = psl(1)
-dta = psl(0)
+dta = psl(0.3)
 modulation_frequency = psl(0)
 stim_rate_mu = psl(80)
 stim_rate_sigma = psl(0)
 noise_rate_mu = psl(10)
 noise_rate_sigma = psl(0)
 n_stim_patterns = psl(128)
-n_trials = psl(60)
-sim_duration = psl(200.0)
-ana_duration = psl(150.0) # must be < min(sim_duration)
-training_size = psl(30) # must be < min(n_trials)
+n_trials = psl(1) # typically, 1 when simulating and 60 when analysing
+sim_duration = psl(3750.) # typically, 3750 when simulating and 180 when analysing
+ana_duration = psl(30.0) # when analysing, must be < min(sim_duration)
+training_size = psl(30) # when analysing, must be < min(n_trials)
 multineuron_metric_mixing = psl(0.)
 linkage_method = psl(1) # 0: ward, 1: kmeans
 tau = psl(5)
 dt = psl(2)
 
 #----parameter consistency check
-if training_size.realstop > n_trials.start:
+if analyse and training_size.realstop > n_trials.start:
     raise Exception("Decoder training set size must always be smaller than the number of trials!")
-if ana_duration.realstop > sim_duration.start:
+if analyse and ana_duration.realstop > sim_duration.start:
     raise Exception("Simulation length must always be greater than analysis time window!")
 
 #----determine which cluster we're runnning on
