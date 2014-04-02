e8fac1919ae8e58a0ffc3b841c802ca17d5713f5
Eugenio Piasini <e.piasini@ucl.ac.uk>
2014-04-02 16:44:40 +0100

    Various cosmetic changes to efficient frontier plotting scripts.

--- scripts/efficient_frontier_plot.py
+++ scripts/efficient_frontier_plot.py
@@ -6,22 +6,25 @@ from matplotlib import pyplot as plt
 import matplotlib.colors as colors
 import matplotlib.cm as cmx
 
-iscs_range = [0,4]
-spn = 1024
+#iscs_range = [0,4]
+iscs = 0
+spn = 128
 ics = 0.0
 ecs = 1.0
-#sm_range = [40, 80, 120]
-sm = 80
+sm_range = [40, 80, 120]
+#sm = 80
 p_mf_range = np.arange(.1,1,.1)
 n_gd_range = np.arange(1,21,1)
-#dta_range = [0.1, 0.3, 1.0]
+#dta_range = [0., 0.3, 1.0]
 dta = 0.
+#adur_range = [15, 30, 45, 60]
+#adur=30
 
 max_mi = np.log2(spn)
 
 fig, ax = plt.subplots(figsize=(6,4.5))
 ax.locator_params(tight=False, nbins=4)
-fig_io, axs_io = plt.subplots(nrows=len(iscs_range), ncols=1, figsize=(6,4), squeeze=False, sharex=True, sharey=True)
+fig_io, axs_io = plt.subplots(nrows=len(sm_range), ncols=1, figsize=(6,4), squeeze=False, sharex=True, sharey=True) # figize 6,4 for figure 8; 3,4.5 for figure S8
 # proxy artists and labels for legend
 artists = []
 labels = []
@@ -39,33 +42,51 @@ cb = matplotlib.colorbar.ColorbarBase(cb_ax, cmap=cm, norm=c_norm_discrete, orie
 cb.set_label('Synaptic connections')
 cb.set_ticks([])
 
-for i, iscs in enumerate(iscs_range):
+for i, sm in enumerate(sm_range):
     mi_data = np.loadtxt('../../data/data_mi_iscs{}_spn{}_dta{:.01f}_ics{:.01f}_ecs{:.01f}_sm{}.csv'.format(iscs, spn, dta, ics, ecs, sm), delimiter=',')
-    a_gc_data = np.loadtxt('../../data/data_a_gc_iscs{}_spn{}_dta{:.01f}_ics{:.01f}_ecs{:.01f}_sm{}.csv'.format(iscs, spn, dta, ics, ecs, sm), delimiter=',')
-    a_mf_data = np.loadtxt('../../data/data_a_mf_iscs{}_spn{}_dta{:.01f}_ics{:.01f}_ecs{:.01f}_sm{}.csv'.format(iscs, spn, dta, ics, ecs, sm), delimiter=',')
-    color = ['k', 'r', 'g'][i]
-    artists.append(matplotlib.patches.Rectangle((0, 0), 1, 1, fc=color))
-    labels.append("{}".format({0:'Uncorrelated', 4:'Correlated'}[iscs]))
+    a_gc_data = np.loadtxt('../../data/data_v_gc_iscs{}_spn{}_dta{:.01f}_ics{:.01f}_ecs{:.01f}_sm{}.csv'.format(iscs, spn, dta, ics, ecs, sm), delimiter=',')
+    a_mf_data = np.loadtxt('../../data/data_v_mf_iscs{}_spn{}_dta{:.01f}_ics{:.01f}_ecs{:.01f}_sm{}.csv'.format(iscs, spn, dta, ics, ecs, sm), delimiter=',')
+    color = 'kgr'[i]
+    marker= '^os'[i]
+    markersize=[10,10,10][i]
+    edge_color = ['#808080', '#004d00', '#4d0000'][i]
+    #artists.append(matplotlib.patches.Rectangle((0, 0), 1, 1, fc=color))
+    #label = "{}".format({0:'Independent', 4:'Correlated'}[iscs])
+    label = "{}".format(sm)
+    #label = "{}".format(dta)
+    #labels.append("{}".format(adur))
+    #sparsification = (1-a_gc_data)/(1-a_mf_data)
+    #ax.plot(sparsification.mean(axis=1), mi_data.mean(axis=1)/max_mi, color=color, linewidth=3)
     for j, gd in enumerate(n_gd_range):
+        color = scalar_map.to_rgba(gd)
         mi = mi_data[j,:].mean()/max_mi
+        #sparsification = 1-a_gc_data[j,:].mean()
         sparsification = ((1-a_gc_data[j,:])/(1-a_mf_data[j,:])).mean()
         if np.isnan(mi):
             print('Found NaN value for DTA={}, gd={}'.format(dta, gd))
         else:
-            ax.plot(sparsification, mi, c=color, markeredgecolor=color, marker=r"${}$".format(gd), markersize=20)
+            if gd == n_gd_range[0]:
+                ax.plot(sparsification, mi, c=scalar_map.to_rgba(gd), marker=marker, markersize=markersize, markeredgewidth=1, linewidth=2, label=label, zorder=6-i)
+            else:
+                ax.plot([prev_sparsification, sparsification], [prev_mi, mi], c=color, linewidth=2, zorder=5-i)
+                ax.plot(sparsification, mi, c=color, marker=marker, markersize=markersize,  markeredgewidth=1, linewidth=2, zorder=6-i)
+        prev_mi = mi
+        prev_sparsification = sparsification
+            #ax.plot(sparsification, mi, c=color, markeredgecolor=edge_color, marker=r"${}$".format(gd), markersize=20)
     # plot mean output spike count vs mean input spike count
     i_mean_count = np.load('../../data/data_i_mean_count_iscs{}_spn{}_dta{:.01f}_ics{:.01f}_ecs{:.01f}_sm{}.npy'.format(iscs, spn, dta, ics, ecs, sm))
     o_mean_count = np.load('../../data/data_o_mean_count_iscs{}_spn{}_dta{:.01f}_ics{:.01f}_ecs{:.01f}_sm{}.npy'.format(iscs, spn, dta, ics, ecs, sm))
     ax_io = axs_io.flat[i]
-    ax_io.locator_params(tight=True, nbins=5)
+    ax_io.locator_params(axis='x', tight=True, nbins=5)
+    ax_io.locator_params(axis='y', tight=True, nbins=4)
     for j, gd in enumerate(n_gd_range):
         c = scalar_map.to_rgba(gd)
         ax_io.plot(i_mean_count[j],
                    o_mean_count[j],
                    color=c,
                    linewidth=1.5)
-
-ax.legend(artists, labels, loc=3)
+ax.legend(loc='lower left', title='MF rate (Hz)')
+#ax.legend(artists, labels, loc='lower left', title='MF rate (Hz)')
 ax.set_xlabel('Average sparsification')
 ax.set_ylabel('Average MI/H(input)')
 axs_io.flat[-1].set_xlabel('Average spikes per MF')
--- scripts/efficient_frontier_theory.py
+++ scripts/efficient_frontier_theory.py
@@ -6,14 +6,18 @@ from matplotlib import pyplot as plt
 import matplotlib.colors as colors
 import matplotlib.cm as cmx
 
-thinning = 1
-thinning_indexes = [0,1,2,3,10,20,40]
+thinning = 10
+thinning_indexes = [0,10,20,40]
 
 nadt_range = np.vstack((np.array([0]).reshape(1,1), np.loadtxt('../../data/theory_efficient_frontier/NADT.csv', delimiter=',').reshape(-1,1)))[thinning_indexes]
-sparsification_nadt = np.loadtxt('../../data/theory_efficient_frontier/meanSpa.csv', delimiter=',')
-sparsification_fixed = np.loadtxt('../../data/theory_efficient_frontier/meanFXDSpa.csv', delimiter=',')
-encodable_range_nadt = np.loadtxt('../../data/theory_efficient_frontier/meanEnt.csv', delimiter=',')/30.
-encodable_range_fixed = np.loadtxt('../../data/theory_efficient_frontier/meanFXDEnt.csv', delimiter=',')/30.
+# sparsification_nadt = np.loadtxt('../../data/theory_efficient_frontier/meanSpa.csv', delimiter=',')
+# sparsification_fixed = np.loadtxt('../../data/theory_efficient_frontier/meanFXDSpa.csv', delimiter=',')
+# encodable_range_nadt = np.loadtxt('../../data/theory_efficient_frontier/meanEnt.csv', delimiter=',')/30.
+# encodable_range_fixed = np.loadtxt('../../data/theory_efficient_frontier/meanFXDEnt.csv', delimiter=',')/30.
+sparsification_nadt = np.loadtxt('../../data/theory_efficient_frontier/encoded_sparsification_NADT.csv', delimiter=',')
+sparsification_fixed = np.loadtxt('../../data/theory_efficient_frontier/encoded_sparsification_fixed.csv', delimiter=',')
+encodable_range_nadt = np.loadtxt('../../data/theory_efficient_frontier/encodable_range_NADT.csv', delimiter=',')
+encodable_range_fixed = np.loadtxt('../../data/theory_efficient_frontier/encodable_range_fixed.csv', delimiter=',')
 
 sparsification = np.vstack((sparsification_fixed, sparsification_nadt))[thinning_indexes]
 encodable_range = np.vstack((encodable_range_fixed, encodable_range_nadt))[thinning_indexes]
@@ -24,7 +28,7 @@ markersize=6
 markeredgewidth=0.45
 
 fig, ax = plt.subplots(figsize=(3.1,2.65))
-ax.locator_params(tight=False, nbins=4)
+ax.locator_params(tight=False, nbins=3)
 
 diverging_colormap='RdYlGn_r'
 cm = plt.get_cmap(diverging_colormap)
@@ -41,26 +45,32 @@ cb.set_ticks([])
 
 for i, nadt in enumerate(nadt_range):
     marker= 'o^sd*h<H<1+x8Dd^os1d*h<H+x8Dd^os1d*h<H+x8Dd'[i]
+    if marker=='s': print nadt, sparsification[i,3], encodable_range[i,3]
     label = "{}".format(nadt)
     for j, gd in enumerate(n_gd_range):
         color = scalar_map.to_rgba(gd)
         if gd == n_gd_range[0]:
             ax.plot(sparsification[i,j], encodable_range[i,j], c=scalar_map.to_rgba(gd), marker=marker, markersize=markersize, markeredgewidth=markeredgewidth, linewidth=2, label=label, zorder=6-i)
         else:
-            if gd % 4 ==0: 
-                ax.plot(sparsification[i,j], encodable_range[i,j], c=scalar_map.to_rgba(gd), marker=marker, markersize=markersize, markeredgewidth=markeredgewidth, linewidth=2, zorder=6-i)
-                if gd == 4:
-                    ax.plot([sparsification[i,0], sparsification[i,j]], [encodable_range[i,0], encodable_range[i,j]], c=color, linewidth=2, zorder=5-i)
-                else:
-                    ax.plot([sparsification[i,j-4], sparsification[i,j]], [encodable_range[i,j-4], encodable_range[i,j]], c=color, linewidth=2, zorder=5-i)
-            else:
-                ax.plot(sparsification[i,j], encodable_range[i,j], c='#808080', alpha=0.4, marker=marker, markersize=markersize, markeredgewidth=0, linewidth=2, zorder=6-i)
+            ax.plot(sparsification[i,j], encodable_range[i,j], c=color, alpha=1.0, marker=marker, markersize=markersize, markeredgewidth=markeredgewidth, zorder=6-i)
+            ax.plot([sparsification[i,j-1], sparsification[i,j]], [encodable_range[i,j-1], encodable_range[i,j]], c=color, linewidth=2, zorder=1-i)
+            # if gd % 4 ==0: 
+            #     ax.plot(sparsification[i,j], encodable_range[i,j], c=scalar_map.to_rgba(gd), marker=marker, markersize=markersize, markeredgewidth=markeredgewidth, zorder=6-i)
+            #     if gd == 4:
+            #         ax.plot([sparsification[i,0], sparsification[i,j]], [encodable_range[i,0], encodable_range[i,j]], c=color, linewidth=2, zorder=5-i)
+            #     else:
+            #         pass
+            #         ax.plot([sparsification[i,j-4], sparsification[i,j]], [encodable_range[i,j-4], encodable_range[i,j]], c=color, linewidth=2, zorder=5-i)
+            # else:
+            #     ax.plot(sparsification[i,j], encodable_range[i,j], c='#808080', alpha=0.4, marker=marker, markersize=markersize, markeredgewidth=0, zorder=6-i)
 #ax.legend(loc='lower left', title='NADT')
 #ax.legend(artists, labels, loc='lower left', title='MF rate (Hz)')
-ax.set_xlabel('Average sparsification')
-ax.set_ylabel('Average entropy/30 bits')
-ax.set_xticks([1,2,3,4])
-ax.set_yticks([0.4,0.6,0.8,1.0])
-ax.set_xlim(0.9,4.05)
-ax.set_ylim(0.2,1.03)
+ax.set_xlabel('Sparsification on s.e.r.')
+ax.set_ylabel('Sparse encodable range')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+#ax.set_xticks([1,2,3,4])
+#ax.set_yticks([0.4,0.6,0.8,1.0])
+#ax.set_xlim(0.9,4.05)
+#ax.set_ylim(0.2,1.03)
 plt.show()
